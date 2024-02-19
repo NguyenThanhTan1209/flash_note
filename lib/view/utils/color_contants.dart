4ac92ddbe8416225d4ca18853db1004d3c160c4d
Tan Nguyen <nguyenthanhtan1298@gmail.com>
2024-02-19 16:06:36 +0700

    feat: Create Add Note UI

---
+++ lib/view/utils/color_contants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ColorContants {
+  static const Color mainColor = Color(0xff7F00FF);
+  static const Color secondaryColor = Color(0xfffff000);
+  static const Color whiteColor = Colors.white;
+}
---
+++ lib/view/utils/number_contants.dart
@@ -0,0 +1,6 @@
+class NumberContants {
+  static const double padding16 = 16.0;
+
+  static const double height16 = 16.0;
+  
+}
